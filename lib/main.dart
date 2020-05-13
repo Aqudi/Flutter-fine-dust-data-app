e8f8217c3ea46db097f8947cf6e6a73ef3c86f17
Aqudi <gjdigj@naver.com>
2020-05-13 16:28:57 +0900

    Update UI to fetch and display data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttermiseapp/models/AirResult.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -22,14 +26,39 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
-  final TextStyle MiseNumberStyle = TextStyle(fontSize: 40);
-  final TextStyle MiseTextStyle = TextStyle(fontSize: 20);
-  final TextStyle MiseOtherInfoTextStyle = TextStyle(fontSize: 16);
+  final int mappedColor = 0;
+  final int mappedStr = 1;
+
+  final TextStyle miseNumberStyle = TextStyle(fontSize: 40);
+  final TextStyle mseTextStyle = TextStyle(fontSize: 20);
+  final TextStyle miseOtherInfoTextStyle = TextStyle(fontSize: 16);
+
+  AirResult _result;
+
+  Future<AirResult> fetchData() async {
+    var response = await http.get(
+        'https://api.airvisual.com/v2/nearest_city?key=bc70748e-9b95-4c94-9bb6-e12030dac2e6');
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    return result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetchData().then((airResult) {
+      setState(() {
+        this._result = airResult;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+      body: _result == null
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
         padding: const EdgeInsets.all(8.0),
         child: Center(
           child: Column(
@@ -37,7 +66,8 @@ class _MainState extends State<Main> {
             children: <Widget>[
               Text(
                 '현재 위치의 미세먼지',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                style:
+                TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -46,48 +76,54 @@ class _MainState extends State<Main> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       Container(
-                        color: Colors.yellow,
+                        color: getMappedData(_result)[mappedColor],
                         padding: const EdgeInsets.all(8),
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          mainAxisAlignment:
+                          MainAxisAlignment.spaceAround,
                           children: <Widget>[
                             Text('얼굴사진'),
                             Text(
-                              '80',
-                              style: MiseNumberStyle,
+                              '${_result.data.current.pollution.aqius}',
+                              style: miseNumberStyle,
                             ),
                             Text(
-                              '보통',
-                              style: MiseTextStyle,
+                              getMappedData(_result)[mappedStr],
+                              style: mseTextStyle,
                             ),
                           ],
                         ),
                       ),
-
                       Container(
                         padding: const EdgeInsets.all(8),
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          mainAxisAlignment:
+                          MainAxisAlignment.spaceAround,
                           children: <Widget>[
                             Row(
                               children: <Widget>[
-                                Text('이미지'),
+                                Image.network(
+                                  'https://airvisual.com/images/${_result.data
+                                      .current.weather.ic}.png',
+                                  width: 32,
+                                  height: 32,
+                                ),
                                 SizedBox(
                                   width: 16,
                                 ),
                                 Text(
-                                  '11',
-                                  style: MiseOtherInfoTextStyle,
+                                  '${_result.data.current.weather.tp}',
+                                  style: miseOtherInfoTextStyle,
                                 ),
                               ],
                             ),
                             Text(
-                              '습도 100%',
-                              style: MiseOtherInfoTextStyle,
+                              '${_result.data.current.weather.hu}%',
+                              style: miseOtherInfoTextStyle,
                             ),
                             Text(
-                              '풍속 5.7m/s',
-                              style: MiseOtherInfoTextStyle,
+                              '${_result.data.current.weather.ws}m/s',
+                              style: miseOtherInfoTextStyle,
                             ),
                           ],
                         ),
@@ -100,7 +136,16 @@ class _MainState extends State<Main> {
                 borderRadius: BorderRadius.circular(30),
                 child: RaisedButton(
                   color: Colors.green,
-                  onPressed: () {},
+                  onPressed: () {
+                    setState(() {
+                      this._result = null;
+                    });
+                    fetchData().then((airResult) {
+                      setState(() {
+                        this._result = airResult;
+                      });
+                    });
+                  },
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
                       vertical: 15.0,
@@ -115,5 +160,26 @@ class _MainState extends State<Main> {
         ),
       ),
     );
+  }
+
+  dynamic getMappedData(AirResult result) {
+    final pollutionData = result.data.current.pollution.aqius;
+
+    final mapping = [
+      [Colors.greenAccent, "좋음"],
+      [Colors.yellow, "보통"],
+      [Colors.orange, "나쁨"],
+      [Colors.red, "매우 나쁨"],
+    ];
+
+    if (pollutionData <= 50) {
+      return mapping[0];
+    } else if (pollutionData <= 100) {
+      return mapping[1];
+    } else if (pollutionData <= 150) {
+      return mapping[2];
+    } else {
+      return mapping[3];
+    }
   }
 }
